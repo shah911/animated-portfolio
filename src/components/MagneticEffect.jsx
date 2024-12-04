d419555888697eadab386b3dff46f92629c60193
shah 911 <mianshahzeb911@gmail.com>
2024-12-05 00:42:12 +0500

    refactor code

--- src/components/MagneticEffect.jsx
+++ src/components/MagneticEffect.jsx
@@ -1,47 +1,31 @@
-import React, { useEffect, useRef } from "react";
-import { motion, useMotionValue, useSpring } from "framer-motion";
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
 
-export default function MagneticEffect({ children }) {
-  const magnetic = useRef(null);
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-  const springX = useSpring(x, { stiffness: 150, damping: 10, mass: 1.25 });
-  const springY = useSpring(y, { stiffness: 150, damping: 10, mass: 1.25 });
+function MagneticEffect({ children }) {
+  const ref = useRef(null);
+  const [cordinates, setCordinates] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    const currentMagnetic = magnetic.current;
-    if (!currentMagnetic) return;
-
-    const mouseMove = (e) => {
-      const { clientX, clientY } = e;
-      const { height, width, left, top } =
-        currentMagnetic.getBoundingClientRect();
-      const xValue = clientX - (left + width / 2);
-      const yValue = clientY - (top + height / 2);
-
-      x.set(xValue);
-      y.set(yValue);
-    };
-
-    const mouseLeave = () => {
-      x.set(0);
-      y.set(0);
-    };
-
-    currentMagnetic.addEventListener("mousemove", mouseMove);
-    currentMagnetic.addEventListener("mouseleave", mouseLeave);
-
-    return () => {
-      if (currentMagnetic) {
-        currentMagnetic.removeEventListener("mousemove", mouseMove);
-        currentMagnetic.removeEventListener("mouseleave", mouseLeave);
-      }
-    };
-  }, [x, y]);
-
+  const onMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const { width, height, left, top } = ref.current.getBoundingClientRect();
+    const x = clientX - (left + width / 2);
+    const y = clientY - (top + height / 2);
+    setCordinates({ x, y });
+  };
+  const onMouseLeave = () => {
+    setCordinates({ x: 0, y: 0 });
+  };
   return (
-    <motion.div ref={magnetic} style={{ x: springX, y: springY }}>
+    <motion.div
+      ref={ref}
+      onMouseMove={onMouseMove}
+      onMouseLeave={onMouseLeave}
+      animate={{ x: cordinates.x, y: cordinates.y }}
+      transition={{ type: "spring", stiffness: 150, damping: 10, mass: 1.25 }}
+    >
       {children}
     </motion.div>
   );
 }
+
+export default MagneticEffect;
